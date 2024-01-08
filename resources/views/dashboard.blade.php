0b504ace1f327253fb564d8bcbb60ab8acd67b6b
widath2 <135122442+widath2@users.noreply.github.com>
2024-01-08 18:58:51 +0530

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+   <div class="vv" style="margin-left:40%; margin-top:10%;"><h1>HI , {{Auth::user()->name}} <h1>
+   <button style="
+  background-color: #4ae;
+  width: 300px;
+  height: 34px; border-radius: 16px;" ><a href="/">Go To Home</a></button></div>
+
+   
+</x-app-layout>
